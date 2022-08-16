219af4efa1f6328150f3c7955da2eb2272daec41
Fabian Hohmuth <fafa3711@users.noreply.github.com>
2022-08-16 13:54:20 +0200

    added bare bones headless version

--- src/JoeScan.LogScanner.Headless/Program.cs
+++ src/JoeScan.LogScanner.Headless/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Autofac;
+using Autofac.Extras.NLog;
+using JoeScan.LogScanner.Core;
+using JoeScan.LogScanner.Core.Models;
+using JoeScan.LogScanner.Replay;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+Console.CancelKeyPress += (_, e) =>
+{
+    Console.WriteLine("Cancelled");
+    autoResetEvent.Set();
+};
+EnableLogging();
+var builder = new ContainerBuilder();
+builder.RegisterModule<ReplayModule>();
+builder.RegisterModule<CoreModule>();
+builder.RegisterModule<NLogModule>();
+
+var container = builder.Build();
+
+using var scope = container.BeginLifetimeScope();
+var engine= scope.Resolve<LogScannerEngine>();
+engine.SetActiveAdapter(engine.AvailableAdapters.First()); // we only have the replay adapter registered
+engine.Start();
+
+autoResetEvent.WaitOne();
+engine.Stop();
+
+
+void EnableLogging()
+ {
+     var config = new LoggingConfiguration();
+     const string layoutString = "[${longdate:useUTC=false}][${level:uppercase=true}]: ${message} ${exception:innerFormat=tostring:maxInnerExceptionLevel=10:separator=,:format=tostring}";
+    
+     var consoleTarget = new ColoredConsoleTarget()
+     {
+         
+         Layout = layoutString,
+         
+     };
+     config.AddTarget("console", consoleTarget);
+     config.LoggingRules.Add(new LoggingRule("*", LogLevel.Trace, consoleTarget));
+
+     LogManager.Configuration = config;
+     LogManager.ReconfigExistingLoggers();
+ }
