1fa10c8e177f4b49e5d314514deb84873466a687
Fabian Hohmuth <fafa3711@users.noreply.github.com>
2022-12-01 10:45:28 +0100

    made log number resettable at runtime

--- src/JoeScan.LogScanner.Core/Models/PieceNumberProvider.cs
+++ src/JoeScan.LogScanner.Core/Models/PieceNumberProvider.cs
@@ -6,31 +6,56 @@ namespace JoeScan.LogScanner.Core.Models;
 
 class PieceNumberProvider : IPieceNumberProvider
 {
+    public event EventHandler<PieceNumberChangedEventArgs> PieceNumberChanged;
+    #region Injected Properties
+
     private ILogger Logger { get; }
-    private static  int nextLogNumber = 0;
-    private readonly FileStream? stream;
+
+    #endregion
+
+    #region Private Fields
+
+    private static int currentLogNumber = 0;
+    private readonly string filePath;
+    private readonly FileSystemWatcher? watcher;
+    private const string fileName = "lastPieceNumber.txt" ;
+    private object locker = new object();
+
+    #endregion
+
+    #region IPieceNumberProvider
 
     public int GetNextPieceNumber()
     {
-        nextLogNumber++;
-        SaveLastNumber(nextLogNumber);
-        return nextLogNumber;
+        currentLogNumber++;
+        SaveLastNumber(currentLogNumber);
+        return currentLogNumber;
     }
+
+    public int PeekNextPieceNumber()
+    {
+        return currentLogNumber+1;
+    }
+
+    public void SetNextPieceNumber(int pieceNumber)
+    {
+        currentLogNumber = pieceNumber;
+        SaveLastNumber(pieceNumber);
+    }
+
+    #endregion
+
+    #region Private Methods
 
     private void SaveLastNumber(int i)
     {
         try
         {
-            if (stream != null)
+            lock (locker)
             {
-                stream.Seek(0L, SeekOrigin.Begin);
-                using (var bw = new BinaryWriter(stream, Encoding.Default, true))
-                {
-                    bw.Write(i);
-                }
-                stream.Flush();
-                Logger.Debug($"Saved last log number successfully.");
+                File.WriteAllText(filePath, i.ToString());
             }
+            Logger.Debug($"Saved last log number {i} successfully.");
         }
         catch (Exception e)
         {
@@ -38,37 +63,107 @@ class PieceNumberProvider : IPieceNumberProvider
         }
     }
 
+    private int GetNumberFromFile()
+    {
+        try
+        {
+            lock (locker)
+            {
+                var str = File.ReadAllText(filePath);
+                if (int.TryParse(str, out int res))
+                {
+                    return res;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Logger.Debug(e);
+        }
+
+        currentLogNumber = 0;
+        return currentLogNumber;
+    }
+
+    #endregion
+
+    #region Lifecycle
+
     public PieceNumberProvider(ILogger logger)
     {
         Logger = logger;
-        // get last number from file
         try
         {
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
             var dirName = Path.Combine(folderPath, "JoeScan", "LogScanner");
-            var fileName = Path.Combine(dirName, "lastPieceNumber.txt");
-
-            logger.Debug($"Trying to read last log number from file: {fileName}");
+            filePath = Path.Combine(dirName, fileName);
+           
+            logger.Debug($"Trying to read last log number from file: {filePath}");
             if (!Directory.Exists(dirName))
             {
                 Directory.CreateDirectory(dirName);
+                SaveLastNumber(0);
             }
-            stream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite); // don't dispose, we keep it open
-            using BinaryReader br = new BinaryReader(stream, Encoding.Default, true);
-            nextLogNumber = br.ReadInt32() ;
+
+            if (!File.Exists(filePath))
+            {
+                SaveLastNumber(0);
+            }
+
+            if (File.Exists(filePath))
+            {
+                watcher = new FileSystemWatcher(dirName);
+                watcher.IncludeSubdirectories = false;
+                watcher.Filter = fileName;
+                watcher.EnableRaisingEvents = true;
+                watcher.Changed += WatcherOnChanged;
+                watcher.Deleted += WatcherOnChanged;
+            }
+
+            currentLogNumber = GetNumberFromFile();
         }
         catch (Exception e)
         {
             logger.Warn($"Reading last log number failed: {e.Message}. Restarting at log #1");
-            nextLogNumber = 0;
+            currentLogNumber = 0;
         }
     }
+
+    private void WatcherOnChanged(object sender, FileSystemEventArgs e)
+    {
+        OnPieceNumberChanged(new PieceNumberChangedEventArgs(GetNumberFromFile()));
+    }
+
+    #endregion
+
+    #region IDisposable Implementation
 
     public void Dispose()
     {
         // we get disposed when the container goes out of scope, i.e. at
         // application shutdown. 
-        stream?.Flush();
-        stream?.Dispose();
+        if (watcher != null)
+        {
+            watcher.Changed -= WatcherOnChanged;
+            watcher.Deleted -= WatcherOnChanged;
+            watcher.Dispose();
+        }
+    }
+
+    #endregion
+
+    protected virtual void OnPieceNumberChanged(PieceNumberChangedEventArgs e)
+    {
+        PieceNumberChanged?.Invoke(this, e);
+    }
+}
+
+public class PieceNumberChangedEventArgs
+{
+    public int NextNumber { get; }
+
+    public PieceNumberChangedEventArgs(int nextNumber)
+    {
+        NextNumber = nextNumber;
     }
 }
